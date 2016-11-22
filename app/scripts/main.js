e1e03ffb8f115ece49098e9174e04a7ff345d7fd
Leo Silverberg <leosilverberg@gmail.com>
2016-11-22 18:01:58 +0000

    the thing with the scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+var fixmeTop = $('.nav').offset().top;       // get initial position of the element
+
+$(window).scroll(function() {                  // assign scroll event listener
+
+    var currentScroll = $(window).scrollTop(); // get current position
+
+    if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+        $('.nav').css({                      // scroll to that element or below it
+            position: 'fixed',
+            top:0,
+
+
+        });
+    } else {                                   // apply position: static
+        $('.nav').css({                      // if you scroll above it
+            position: 'absolute',
+            top:'inherit'
+        });
+    }
+
+});
